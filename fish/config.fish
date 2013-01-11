963b6f2525d6a99ca7448d4d4cb830b2f149ded9
U0 <rhyzix@gmail.com>
2013-01-11 16:59:08 +0800

    only exsist path can add to fish path:fish:

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,6 @@ set NPM_PATH (npm prefix -g)
 
 
 #node.js global modules path
-set -x NODE_PATH $NPM_PATH/lib/node_modules
+set NODE_PATH $NPM_PATH/lib/node_modules
 #PATH
-set -x PATH ~/.config/script $BREW_PATH/bin $BREW_PATH/sbin $NPM_PATH/bin $PATH
-
-
+set PATH ~/.config/script $BREW_PATH/bin $NPM_PATH/bin $PATH
