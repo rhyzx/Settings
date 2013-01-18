a829f151f24197a9ef1ad6d72e05a13b618c09b9
U0 <rhyzix@gmail.com>
2013-01-18 17:00:25 +0800

    update git stage char to emoji:tree:

--- fish/config.fish
+++ fish/config.fish
@@ -2,24 +2,33 @@ set fish_greeting ""
 
 
 
-#brew path
+# brew path
 set BREW_PATH (brew --prefix)
 #npm path
 set NPM_PATH (npm prefix -g)
 
 
-#node.js global modules path
+# node.js global modules path
 set -gx NODE_PATH $NPM_PATH/lib/node_modules
 #PATH
 set PATH ~/.config/script $BREW_PATH/bin $NPM_PATH/bin $PATH
 
 
-#git branch prompt config
+# git branch prompt config
 set __fish_git_prompt_showdirtystate true
 set __fish_git_prompt_showstashstate true
-# set __fish_git_prompt_showuntrackedfiles true
-# set __fish_git_prompt_showupstream true
+#set __fish_git_prompt_showuntrackedfiles true
+#set __fish_git_prompt_showupstream true
 
+# git state color
+# prefix/suffix/bare/merging/branch/stagedstate/invalidstate/untrackedfiles/upstream
 set __fish_git_prompt_color blue
-set __fish_git_prompt_color_dirtystate red
-set __fish_git_prompt_color_stashstate magenta
+#set __fish_git_prompt_color_dirtystate red
+#set __fish_git_prompt_color_stashstate magenta
+
+# git state char
+# upstream_equal/ahead/behind/diverged
+set ___fish_git_prompt_char_dirtystate '👾 '
+set ___fish_git_prompt_char_stashstate '🍳 '
+set ___fish_git_prompt_char_stagedstate '🌀 '
+
