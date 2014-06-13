9b9dbbfbb6ba19373354c62b185fd429bd90b90e
U0 <rhyzix@gmail.com>
2014-06-14 01:56:29 +0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,50 +1,78 @@
 # fish_user_paths does not override path in current version
 # it will be fixed in next release
 # @see https://github.com/fish-shell/fish-shell/issues/1099
-set PATH ~/.config/script /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin /Users/rhyzx/.rbenv/shims /usr/bin /bin /usr/sbin /sbin 
+set PATH ~/.config/scripts /usr/local/bin /usr/local/sbin /usr/local/var/ruby/bin /usr/bin /bin /usr/sbin /sbin
 
 
-# improve speed by confiuring one time
+# improve speed by cache confiuration
 # reload by `set -eU FISHING`
 if not set -qU FISHING
 set -U FISHING 1
+set fish_greeting ""
 
-set -U fish_greeting ""
+set -l p ~/.config/scripts
 
+# TODO
+# check exists
 
-# tmp vars
+# brew (main)
 set -l brew (brew --prefix)
+set p $p $brew/bin $brew/sbin
+
+
+
+# nodejs/npm
 set -l npm (npm prefix -g)
-set -l rbenv (rbenv root)
 
-
-# prepend PATH
-set -U fish_user_paths ~/.config/script $brew/bin $brew/sbin $npm/bin $rbenv/shims
-
-# Perl5 lib for git svn
-set -Ux PERL5LIB "/Applications/Xcode.app/Contents/Developer/Library/Perl/5.16/darwin-thread-multi-2level"
-# node.js global modules path
+## nodejs global modules path
 set -Ux NODE_PATH $npm/lib/node_modules
+
+
+
+# ruby
+## shared gems install path
+set -Ux GEM_HOME /usr/local/var/ruby
+set -Ux GEM_PATH $GEM_HOME
+set p $p $GEM_PATH/bin
+
+# ## rbenv
+# set -Ux RBENV_ROOT /usr/local/var/rbenv
+# set -l rbenv (rbenv root)
+
+
+
+
 # python modules
 set -Ux PYTHONPATH $brew/lib/python2.7/site-packages
 
 
-# git branch prompt config
+
+# Perl5 lib for git svn
+set -Ux PERL5LIB "/Applications/Xcode.app/Contents/Developer/Library/Perl/5.16/darwin-thread-multi-2level"
+
+
+
+# git
+## git branch prompt config
 set -U __fish_git_prompt_showdirtystate true
 set -U __fish_git_prompt_showstashstate true
 #set -U __fish_git_prompt_showuntrackedfiles true
 #set -U __fish_git_prompt_showupstream true
 
-# git state color
-# prefix/suffix/bare/merging/branch/stagedstate/invalidstate/untrackedfiles/upstream
+## git state color
+## prefix/suffix/bare/merging/branch/stagedstate/invalidstate/untrackedfiles/upstream
 set -U __fish_git_prompt_color magenta
 #set -U __fish_git_prompt_color_dirtystate red
 #set -U __fish_git_prompt_color_stashstate blue
 
-# git state char
-# upstream_equal/ahead/behind/diverged
+## git state char
+## upstream_equal/ahead/behind/diverged
 set -U ___fish_git_prompt_char_dirtystate '👾 '
 set -U ___fish_git_prompt_char_stashstate '🍳 '
 set -U ___fish_git_prompt_char_stagedstate '🌀 '
 
+
+# prepend to PATH
+set fish_user_paths $p
 end
+
